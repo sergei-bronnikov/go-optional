b3f6255deca99e3645b083b2be68389e10ff2e43
Sergei Bronnikov <sergeibronnikov87@gmail.com>
2025-12-05 13:28:18 +0000

    readme. doc

--- README.md
+++ README.md
@@ -1,1 +1,109 @@
 # go-optional
+
+A Go implementation of the Optional pattern using generics, inspired by Java's Optional class. This library provides a type-safe way to handle optional values and avoid null pointer dereferences.
+
+## Features
+
+- Type-safe optional values using Go generics
+- Familiar API for developers coming from Java
+- Zero dependencies
+- Simple and intuitive interface
+
+## Installation
+
+```bash
+go get github.com/sergei-bronnikov/go-optional
+```
+
+## Usage
+
+### Creating Optional Values
+
+```go
+import "github.com/sergei-bronnikov/go-optional"
+
+// Create an Optional with a value
+opt := optional.Of("hello")
+
+// Create an Optional from a pointer (can be nil)
+var ptr *string
+opt := optional.OfNullable(ptr)
+
+// Create an empty Optional
+opt := optional.Empty[string]()
+```
+
+### Checking for Values
+
+```go
+opt := optional.Of(42)
+
+if opt.IsPresent() {
+    fmt.Println("Value is present")
+}
+
+if opt.IsEmpty() {
+    fmt.Println("Value is empty")
+}
+```
+
+### Getting Values
+
+```go
+opt := optional.Of("hello")
+
+// Get with boolean indicator
+value, ok := opt.Get()
+if ok {
+    fmt.Println(value)
+}
+
+// Get with default value
+value := opt.OrElse("default")
+```
+
+### Comparing Optionals
+
+```go
+opt1 := optional.Of(42)
+opt2 := optional.Of(42)
+opt3 := optional.Empty[int]()
+
+opt1.Equals(opt2) // true
+opt1.Equals(opt3) // false
+```
+
+### String Representation
+
+```go
+opt := optional.Of("hello")
+fmt.Println(opt.String()) // Output: Optional[hello]
+
+empty := optional.Empty[string]()
+fmt.Println(empty.String()) // Output: Optional.empty
+```
+
+## API Reference
+
+### Types
+
+- `Optional[T any]` - A container object which may or may not contain a value
+
+### Functions
+
+- `Of[T any](val T) Optional[T]` - Creates an Optional with the given non-nil value
+- `OfNullable[T any](val *T) Optional[T]` - Creates an Optional from a pointer, which may be nil
+- `Empty[T any]() Optional[T]` - Returns an empty Optional
+
+### Methods
+
+- `IsPresent() bool` - Returns true if a value is present
+- `IsEmpty() bool` - Returns true if no value is present
+- `Get() (T, bool)` - Returns the value if present, and a boolean indicating presence
+- `OrElse(other T) T` - Returns the value if present, otherwise returns the provided default
+- `Equals(other Optional[T]) bool` - Compares two Optionals for equality
+- `String() string` - Returns a string representation of the Optional
+
+## License
+
+See LICENSE file for details.
--- optional.go
+++ optional.go
@@ -1,3 +1,15 @@
+// Package optional provides a type-safe implementation of the Optional pattern
+// using Go generics. It offers a container object which may or may not contain
+// a value, helping to avoid null pointer dereferences and making the presence
+// or absence of values explicit.
+//
+// Example usage:
+//
+//	opt := optional.Of("hello")
+//	if opt.IsPresent() {
+//	    value, _ := opt.Get()
+//	    fmt.Println(value)
+//	}
 package optional
 
 import (
@@ -5,14 +17,32 @@ import (
 	"reflect"
 )
 
+// Optional is a container object which may or may not contain a value.
+// If a value is present, IsPresent() returns true. If no value is present,
+// IsEmpty() returns true.
 type Optional[T any] struct {
 	value *T
 }
 
+// Of returns an Optional containing the given value.
+//
+// Example:
+//
+//	opt := optional.Of("hello")
 func Of[T any](val T) Optional[T] {
 	return Optional[T]{value: &val}
 }
 
+// OfNullable returns an Optional containing the value pointed to by val if val is not nil,
+// otherwise returns an empty Optional.
+//
+// Example:
+//
+//	var ptr *string = nil
+//	opt := optional.OfNullable(ptr) // empty Optional
+//
+//	str := "hello"
+//	opt = optional.OfNullable(&str) // Optional with "hello"
 func OfNullable[T any](val *T) Optional[T] {
 	if val == nil {
 		return Optional[T]{value: nil}
@@ -20,18 +50,50 @@ func OfNullable[T any](val *T) Optional[T] {
 	return Optional[T]{value: val}
 }
 
+// Empty returns an empty Optional instance.
+//
+// Example:
+//
+//	opt := optional.Empty[string]()
 func Empty[T any]() Optional[T] {
 	return Optional[T]{value: nil}
 }
 
+// IsPresent returns true if a value is present, otherwise false.
+//
+// Example:
+//
+//	opt := optional.Of(42)
+//	if opt.IsPresent() {
+//	    fmt.Println("Value exists")
+//	}
 func (o *Optional[T]) IsPresent() bool {
 	return o.value != nil
 }
 
+// IsEmpty returns true if no value is present, otherwise false.
+//
+// Example:
+//
+//	opt := optional.Empty[string]()
+//	if opt.IsEmpty() {
+//	    fmt.Println("No value")
+//	}
 func (o *Optional[T]) IsEmpty() bool {
 	return o.value == nil
 }
 
+// Get returns the value if present, along with a boolean indicating whether
+// the value was present. If no value is present, returns the zero value for type T
+// and false.
+//
+// Example:
+//
+//	opt := optional.Of("hello")
+//	value, ok := opt.Get()
+//	if ok {
+//	    fmt.Println(value)
+//	}
 func (o *Optional[T]) Get() (T, bool) {
 	if o.value != nil {
 		return *o.value, true
@@ -40,6 +102,15 @@ func (o *Optional[T]) Get() (T, bool) {
 	return zero, false
 }
 
+// OrElse returns the value if present, otherwise returns the provided default value.
+//
+// Example:
+//
+//	opt := optional.Empty[string]()
+//	value := opt.OrElse("default") // returns "default"
+//
+//	opt = optional.Of("hello")
+//	value = opt.OrElse("default") // returns "hello"
 func (o *Optional[T]) OrElse(other T) T {
 	if o.value != nil {
 		return *o.value
@@ -47,6 +118,15 @@ func (o *Optional[T]) OrElse(other T) T {
 	return other
 }
 
+// Equals compares this Optional with another Optional for equality.
+// Two Optionals are considered equal if both are empty, or if both contain
+// values that are deeply equal.
+//
+// Example:
+//
+//	opt1 := optional.Of(42)
+//	opt2 := optional.Of(42)
+//	opt1.Equals(opt2) // returns true
 func (o *Optional[T]) Equals(other Optional[T]) bool {
 	if o.IsPresent() && other.IsPresent() {
 		return reflect.DeepEqual(*o.value, *other.value)
@@ -54,6 +134,14 @@ func (o *Optional[T]) Equals(other Optional[T]) bool {
 	return o.IsEmpty() && other.IsEmpty()
 }
 
+// String returns a string representation of the Optional.
+// If a value is present, returns "Optional[value]".
+// If no value is present, returns "Optional.empty".
+//
+// Example:
+//
+//	opt := optional.Of("hello")
+//	fmt.Println(opt.String()) // Output: Optional[hello]
 func (o *Optional[T]) String() string {
 	if o.value != nil {
 		return fmt.Sprintf("Optional[%v]", *o.value)
